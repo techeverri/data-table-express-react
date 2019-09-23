8d07df058a7dfe2d33ed06e577ec52dcbb4e16de
Tomas Echeverri Valencia <tomechval@gmail.com>
2019-09-23 16:25:11 +0200

    Add basic express server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const PORT = process.env.PORT || 9000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get('/ping', (req, res) => res.send('pong'));
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server ready on port ${PORT}`);
+});
