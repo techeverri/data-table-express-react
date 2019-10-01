fdb95540e93728eb3ee9c4df220a0225dbf40bdc
Tomas Echeverri Valencia <tomechval@gmail.com>
2019-10-01 21:53:56 +0200

    Create dataController

---
+++ server/controllers.js
@@ -0,0 +1,54 @@
+import fetch from 'node-fetch';
+import { API_URL } from './config';
+import Fuse from 'fuse.js';
+
+export const dataController = async (req, res) => {
+  const { page, limit, search, filterBy, sortBy, order } = req.query;
+
+  let data;
+
+  try {
+    const result = await fetch(API_URL);
+    data = (await result.json()).output;
+  } catch (response) {
+    return res.sendStatus(500);
+  }
+
+  let statuses = data.map(entry => entry.status);
+  statuses = [...new Set(statuses)].sort();
+
+  if (filterBy) {
+    data = data.filter(entry => entry.status === filterBy);
+  }
+
+  if (search) {
+    const options = { keys: ['name'], threshold: 0.4 };
+    const fuse = new Fuse(data, options);
+
+    data = fuse.search(search);
+  }
+
+  if (sortBy) {
+    data = data.sort(
+      (a, b) => (a[sortBy] > b[sortBy] ? 1 : -1) * (order === 'asc' ? 1 : -1)
+    );
+  }
+
+  const start = (page - 1) * limit;
+  const end = page * limit;
+  const entries = data.slice(start, end);
+
+  const total = data.length;
+  const pages = Math.ceil(total / limit);
+
+  return res.json({
+    entries,
+    limit,
+    page,
+    from: start + 1,
+    to: end,
+    total,
+    pages,
+    statuses,
+  });
+};
--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import path from 'path';
-import fetch from 'node-fetch';
-import { API_URL } from './config';
-import Fuse from 'fuse.js';
+import { dataController } from './controllers';
 
 const app = express();
 
@@ -16,56 +14,7 @@ app.get('/', (req, res) => {
 
 app.get('/ping', (req, res) => res.send('pong'));
 
-app.get('/data', async (req, res) => {
-  const { page, limit, search, filterBy, sortBy, order } = req.query;
-
-  let data;
-
-  try {
-    const result = await fetch(API_URL);
-    data = (await result.json()).output;
-  } catch (response) {
-    return res.sendStatus(500);
-  }
-
-  let statuses = data.map(entry => entry.status);
-  statuses = [...new Set(statuses)].sort();
-
-  if (filterBy) {
-    data = data.filter(entry => entry.status === filterBy);
-  }
-
-  if (search) {
-    const options = { keys: ['name'], threshold: 0.4 };
-    const fuse = new Fuse(data, options);
-
-    data = fuse.search(search);
-  }
-
-  if (sortBy) {
-    data = data.sort(
-      (a, b) => (a[sortBy] > b[sortBy] ? 1 : -1) * (order === 'asc' ? 1 : -1)
-    );
-  }
-
-  const start = (page - 1) * limit;
-  const end = page * limit;
-  const entries = data.slice(start, end);
-
-  const total = data.length;
-  const pages = Math.ceil(total / limit);
-
-  return res.json({
-    entries,
-    limit,
-    page,
-    from: start + 1,
-    to: end,
-    total,
-    pages,
-    statuses,
-  });
-});
+app.get('/data', dataController);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server ready on port ${PORT}`);
